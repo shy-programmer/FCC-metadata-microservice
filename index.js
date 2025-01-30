0ebe346884f4c6784fe1d946a730bf2dcdd07675
Shy programmer <abdulazeezarowolo@gmail.com>
2025-01-30 15:44:07 +0000

    freecodecamp challenge

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+let entryFile = req.file
+if (entryFile) {
+  res.json({
+    name: entryFile.originalname,
+    type: entryFile.mimetype,
+    size: entryFile.size
+  })
+}
+else {res.json('Input a file')}
+})
 
 
 const port = process.env.PORT || 3000;
